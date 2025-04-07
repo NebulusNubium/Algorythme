4a75ff0ee6e403ae4b46d658724252bb22c2b39e
Théo <t.knopfer@gmail.com>
2025-04-07 16:04:36 +0200

    foobar

---
+++ exo_8.js
@@ -0,0 +1,4 @@
+let b = 14
+let c = 10
+let a = Math.sqrt(b * b + c * c);
+console.log("La longueur de l'hypoténuse est : " + a);
--- fonctionPythagore.js
+++
@@ -1,4 +0,0 @@
-let b = "nombre B"
-let c = "nombre C"
-let a = Math.sqrt(b * b + c * c);
-console.log("La longueur de l'hypoténuse est : " a);
